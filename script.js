a61fbbf8d00ce7a4644760d3a3ba47f3a00ab62b
AMN2003 <116036036+AMN2003@users.noreply.github.com>
2025-08-11 19:46:33 +0300

    Added the Hovering effect, Hovering the mouse over the grid changes grid color to blue and on exit it changes back again to the original color

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ function createGrid(size){
     for (let i = 0; i < size * size; i++){
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
+        gridSquare.addEventListener("mouseover", () => {
+            gridSquare.style.backgroundColor = "blue";  
+        });
+        gridSquare.addEventListener("mouseleave", () => {
+            gridSquare.style.backgroundColor = "white";
+        });
         gridContainer.appendChild(gridSquare);
 
     }
